8c04409d7877d6f69b97706184f01e55f3d9cc76
John Rinehart <johnrichardrinehart@gmail.com>
2024-11-23 10:08:17 -0800

    feat: add rust-analyzer to dev path

--- flake.nix
+++ flake.nix
@@ -9,10 +9,15 @@
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
   let
     pkgs = (import nixpkgs { inherit system; } );
-  in
-  {
+  in rec {
     packages = {
       default = pkgs.callPackage ./package.nix {};
+    };
+    devShells = {
+      default = pkgs.mkShell {
+        inputsFrom = [ packages.default ];
+        nativeBuildInputs = [ pkgs.rust-analyzer ];
+      };
     };
   });
 }
